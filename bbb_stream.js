15d22b7246cd72b89972a177a3ccce77a2bba651
Arunkumar Bhat <77152354+arunkumar141997@users.noreply.github.com>
2021-05-22 16:42:09 +0530

    update

--- bbb_stream.js
+++ bbb_stream.js
@@ -16,7 +16,7 @@ var VIEWER_URL = process.env.VIEWER_URL
 let api = bbb.api(BBB_URL, BBB_SECRET)
 let http = bbb.http
 var disp_num = Math.floor(Math.random() * (200 - 99) + 99);
-var xvfb        = new Xvfb({
+var xvfb = new Xvfb({
     displayNum: disp_num,
     silent: true,
     xvfb_args: ["-screen", "0", "1280x800x24", "-ac", "-nolisten", "tcp", "-dpi", "96", "+extension", "RANDR"]
