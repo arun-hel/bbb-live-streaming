211e86db2fe5370f9165fa79edf2d5cefb46de54
Arunkumar Bhat <77152354+arunkumar141997@users.noreply.github.com>
2021-05-22 15:37:29 +0530

    Updated bbb-streming -install.sh

--- bbb-streaming-install.sh
+++ bbb-streaming-install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "Installing bbb-live-streming"
-URL="$(bbb-conf --secret |grep "URL" | cut -d "/" -f3)"
-SECRET="$(bbb-conf --secret |grep "Secret" |head -1| cut -d ":" -f2)"
+URL="$(bbb-conf --secret |grep "URL" | awk '{print $2}')"
+SECRET="$(bbb-conf --secret |grep "Secret"  | head -1| awk '{print $2}' )"
 echo "Generating .env"
 cp -r sample-env .env
-sed -i 's/BBB_URL=.*/$URL/g' .env
-sed -i 's/BBB_SECRET=.*/$SECRET/g' .env
+sed -i 's/BBB_URL=.*/BBB_URL=$URL/g' .env
+sed -i 's/BBB_SECRET=.*/BBB_SECRET=$SECRET/g' .env
 echo "Creating bbb-live-streaming:v1 Docker image"
 docker build -t bbb-live-streaming:v1 .
 echo "Cleaning up the folder"
